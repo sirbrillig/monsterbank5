a0b912f9143fecddadd0e5a4e9fe372a6064475c
Payton Swick <payton@foolord.com>
2014-10-02 19:12:36 -0400

    Add senses to schema

--- app/schemas/monster.js
+++ app/schemas/monster.js
@@ -12,6 +12,7 @@ var MonsterSchema = new Schema({
 	armor: String, // one of 'padded', 'leather', 'studded leather'... 'splint mail', 'plate mail'
 	shield: { type: Boolean, default: false },
 	naturalArmor: String, // same as (and replaces) armor, but will be reported as 'natural armor'
+	senses: String,
 	resistances: [String],
 	immunities: [String],
 	vulnerabilities: [String],
