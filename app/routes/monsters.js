5a937272b9af9fea1d1f071037c6d89efd76a2fb
Payton Swick <payton@foolord.com>
2014-10-02 23:17:24 -0400

    Prevent duplicate named monsters

--- app/routes/monsters.js
+++ app/routes/monsters.js
@@ -4,7 +4,7 @@ var monsterData = require( '../../app/schemas/monster' );
 
 monsterRoutes.list( function( request, response ) {
 	monsterData.find( function( err, monsters ) {
-		if ( err ) response.send( err );
+		if ( err ) return response.send( err );
 		console.log( 'list request' );
 		response.json( monsters.map( function( monster ) {
 			return monster.toJSON( { virtuals: true } );
@@ -14,7 +14,6 @@ monsterRoutes.list( function( request, response ) {
 
 monsterRoutes.get( function( request, response ) {
 	monsterData.findById( request.params.id, function( err, monster ) {
-		if ( err ) response.send( err );
 		if ( ! monster ) {
 			console.warn( 'invalid get request: no such monster', request.params.id );
 			response.statusCode = 404;
@@ -31,23 +30,33 @@ monsterRoutes.create( function( request, response ) {
 		response.statusCode = 400;
 		return response.send( 'Error: 400: Invalid data.' );
 	}
-	console.log( 'create request', request.body );
-	var monster = new monsterData();
-	var keys = Object.getOwnPropertyNames( monster.schema.paths ).filter( function( key ) {
-		return ( key[0] !== '_' );
-	} );
-	keys.forEach( function( key ) {
-		if ( request.body.hasOwnProperty( key ) ) monster[ key ] = request.body[ key ];
-	} );
-	monster.save( function( err ) {
-		if ( err ) response.send( err );
-		response.json( monster.toJSON( { virtuals: true } ) );
+
+	var findPromise = monsterData.findOne( { 'name': request.body.name } ).exec();
+
+	findPromise.then( function( err, monster ) {
+		if ( err || monster ) {
+			console.warn( 'invalid create request: existing name', request.body.name );
+			response.statusCode = 400;
+			return response.send( 'Error: 400: That monster already exists.' );
+		}
+
+		console.log( 'create request', request.body );
+		monster = new monsterData();
+		var keys = Object.getOwnPropertyNames( monster.schema.paths ).filter( function( key ) {
+			return ( key[0] !== '_' );
+		} );
+		keys.forEach( function( key ) {
+			if ( request.body.hasOwnProperty( key ) ) monster[ key ] = request.body[ key ];
+		} );
+		monster.save( function( err ) {
+			if ( err ) return response.send( err );
+			response.json( monster.toJSON( { virtuals: true } ) );
+		} );
 	} );
 });
 
 monsterRoutes.update( function( request, response ) {
 	monsterData.findById( request.params.id, function( err, monster ) {
-		if ( err ) response.send( err );
 		if ( ! monster ) {
 			console.warn( 'invalid update request: no such monster', request.params.id, request.body );
 			response.statusCode = 404;
@@ -61,7 +70,7 @@ monsterRoutes.update( function( request, response ) {
 			if ( request.body.hasOwnProperty( key ) ) monster[ key ] = request.body[ key ];
 		} );
 		monster.save( function( err ) {
-			if ( err ) response.send( err );
+			if ( err ) return response.send( err );
 			response.json( monster.toJSON( { virtuals: true } ) );
 		} );
 	} );
@@ -71,7 +80,7 @@ monsterRoutes.delete( function( request, response ) {
 	monsterData.remove( {
 		_id: request.params.id
 	}, function( err, monster ) {
-		if ( err ) response.send( err );
+		if ( err ) return response.send( err );
 		if ( ! monster ) {
 			console.warn( 'invalid delete request: no such monster', request.params.id );
 			response.statusCode = 404;
